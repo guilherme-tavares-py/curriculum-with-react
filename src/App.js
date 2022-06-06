85b60ab4a8307d700862776a663be3b1938291b6
Guilherme Tavares Pinheiro <gui.tavares2002.contato@gmail.com>
2022-06-06 12:23:28 -0300

    Update Title.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
 import PersonalInformation from "./components/PersonalInformation"
-
+import Title from "./components/Title"
 
 export default function App() {
     return (
         <div>
             <PersonalInformation />
+            <Title  />
         </div>
     )
 }
---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Title() {
+    return (
+        <div className="title">
+            <div className="title-content">
+                <h1>Monica Turner</h1>
+                <h3>Product Manager</h3>
+            </div>
+        </div>
+    )
+}
